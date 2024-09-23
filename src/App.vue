8d53997b2707dbda45e1415a6d48eeebcbf05155
jessReyesM <l211130251@gamadero.tecnm.mx>
2024-09-23 18:52:38 +0000

    FEAT: id of item

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,25 @@
 import { ref } from "vue";
 //modelo 
 const header = ref('App lista de Compras');
+const items = ref([
+  {id:'0', label:'10 manzanas'}, 
+  {id:'1', label:'1 leche'}, 
+  {id:'2', label:'2 lata de atun'},
+  {id:'3', label:'1 mermelada'},
+ {id:'4', label:'1 cajeta'},
+  {id:'5', label:'5 bolillos'}
+]);
 </script>
 
 <template>
 <h1>
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
-    </h1>
+  </h1>
+
+  <ul>
+    <li v-for="item in items" :key="item.id"> 🛒{{ item.label }}</li>
+  </ul>
 </template>
 
 <style scoped>
