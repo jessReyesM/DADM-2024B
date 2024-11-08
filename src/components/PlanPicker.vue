ac41b9a7b1f7842020087235effb988988b1fbaf
jessReyesM <l211130251@gamadero.tecnm.mx>
2024-11-08 17:01:37 +0000

    ✨cambio PlanPickerItem

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,12 +1,12 @@
 <script setup>
-import PlanItem from './PlantItem.vue';
+import PlanPickerItem from './PlanPickerItem.vue';
 import {ref} from 'vue';
 const plans= ref(['El cafetero', 'El chelero', 'El intelectual']);
 </script>
 
 <template>
     <div class="plans">
-      <PlanItem v-for="plan in plans" :key="plan":name="plan" />
+      <PlanPickerItem v-for="plan in plans" :key="plan":name="plan" />
     </div>
 </template>
 
