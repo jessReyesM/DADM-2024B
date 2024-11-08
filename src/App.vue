f86cd2ed02a5ec2c6e6a5d577df3a9efcc72e030
jessReyesM <l211130251@gamadero.tecnm.mx>
2024-11-08 16:40:20 +0000

    ✨S03S04 componente PlanPicker

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-import {ref} from 'vue';
-import PlanItem from './components/plant-item.vue';
-const plans= ref(['El cafetero', 'El chelero', 'El intelectual']);
+import PlanPicker from './components/PlanPicker.vue'; 
 </script>
 
 <template>
@@ -19,12 +17,8 @@ const plans= ref(['El cafetero', 'El chelero', 'El intelectual']);
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
 
-    <div class="plans">
-      <plan-item 
-      v-for="plan in plans" 
-      :key="plan"
-      :name="plan" />
-    </div>
+   <!--todo include picker-->
+    <PlanPicker />
   </div>
 </template>
 
---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,15 @@
+<script setup>
+import PlanItem from './plant-item.vue';
+import {ref} from 'vue';
+const plans= ref(['El cafetero', 'El chelero', 'El intelectual']);
+</script>
+
+<template>
+    <div class="plans">
+      <plan-item v-for="plan in plans" :key="plan":name="plan" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
