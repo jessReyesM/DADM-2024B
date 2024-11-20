eb19434fae1b19ca360dbad7cb116bb1c49f3d21
jessReyesM <l211130251@gamadero.tecnm.mx>
2024-11-20 18:51:54 +0000

    🧊s03s06 hooks

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,6 +1,6 @@
 <script setup>
 import PlanPickerItem from './PlanPickerItem.vue';
-import {ref} from 'vue';
+import {ref, onMounted } from 'vue';
 //creando un areferencia reactiva para almacenar el plan seleccionado
 const selectedPlan = ref(null);
 const plans= ref(['El cafetero', 'El chelero', 'El intelectual']);
@@ -8,6 +8,13 @@ const selectPlan = (name) => {
   //alamcenar el plan seleccionado
    selectedPlan.value = name;
 };
+//intentando obtener la referencia al elemento .plans
+console.log("termina de ejecutar el scrip setup");
+//registrando el hook mounted
+onMounted(() ==> {
+   console.log("el componente  PlanPicker se ha montado 🎈");
+   console.log(document.querySelector('.plans'));
+});
 </script>
 
 <template>
