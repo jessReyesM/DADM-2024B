066aaaca9a5e9928f488c7dfcf2e1bfabff577d2
jessReyesM <l211130251@gamadero.tecnm.mx>
2024-11-27 17:34:50 +0000

    ✨mounted

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,6 +1,6 @@
 <script setup>
 import PlanPickerItem from './PlanPickerItem.vue';
-import {ref, onMounted } from 'vue';
+import {ref, onMounted, onUnmounted } from 'vue';
 //creando un areferencia reactiva para almacenar el plan seleccionado
 const selectedPlan = ref(null);
 const plans= ref(['El cafetero', 'El chelero', 'El intelectual']);
@@ -8,17 +8,26 @@ const selectPlan = (name) => {
   //alamcenar el plan seleccionado
    selectedPlan.value = name;
 };
+
+const plansWrapper = ref(null);
+
 //intentando obtener la referencia al elemento .plans
 console.log("termina de ejecutar el scrip setup");
+
 //registrando el hook mounted
-onMounted(() ==> {
+onMounted(() => {
    console.log("el componente  PlanPicker se ha montado 🎈");
-   console.log(document.querySelector('.plans'));
+   console.log(plansWrapper.value);
+});
+
+//registrando el hook mounted
+onUnmounted(()=>{
+  console.log("El componente PlanPicker se ha desmontado");
 });
 </script>
 
 <template>
-    <div class="plans">
+    <div ref= "plansWrapper" class="plans">
       <PlanPickerItem 
       v-for="plan in plans" 
       :key="plan" 
