6e53864652a07462c5c9a1f6c8813175b0ed259d
jessReyesM <l211130251@gamadero.tecnm.mx>
2024-12-02 01:08:21 +0000

    ✨slots

---
+++ src/components/FancyButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button 
+    @mouseenter="hover = true"
+    @mouseleave="hover = false"> 
+    <slot name="icon" :hover="hover">➡️</slot>
+    <slot>Submit</slot>
+  </button>
+  {{ hover }}
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const hover = ref(false);
+</script>
+
+<style scoped>
+.content{
+  text-align: center;
+}
+button{
+  width: 12rem;
+  margin: 30px auto;
+  font-size: 2rem;
+}
+</style>
